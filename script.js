b4a14ba1e5b4e1a9b63eeba9c39e0817c41e60a9
mercadojesus <mercado_jesus@outlook.es>
2024-03-19 19:46:24 -0300

    23° Actualizacion Proyecto Taller

--- script.js
+++ script.js
@@ -93,3 +93,23 @@ function setMini9(pos) {
         imgProducto.src = "assets/imgs/02-shop/08-6.png";
     }
 }
+
+//  MENÚ RESPONSIVE
+//  Variable que controla si el menú responsive está visible o no
+let menu_responsive_visible = false;
+let nav_responsive = document.getElementById("nav-responsive");
+let close_responsive = document.getElementById("close-responsive");
+let nav = document.getElementById("nav");
+
+nav_responsive.onclick = function() {
+    if(menu_responsive_visible==false){
+        nav.className = "menu-responsive";
+        menu_responsive_visible = true;
+    }
+}
+close_responsive.onclick = function() {
+    if(menu_responsive_visible==true){
+        nav.className = "";
+        menu_responsive_visible = false;
+    }
+}
