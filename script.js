5ed60d12f36d83d78f5ae6d56b8711290011ce6d
mercadojesus <mercado_jesus@outlook.es>
2024-03-18 22:31:13 -0300

    21° Actualizacion Proyecto Taller

---
+++ script.js
@@ -0,0 +1,95 @@
+// Imagen que queremos modificar
+let imgProducto = document.getElementById("imgProducto");
+// Miniaturas
+let miniaturas = document.querySelectorAll(".miniaturas img")
+
+function setMini(pos){
+    if(pos=='0'){
+        imgProducto.src = "assets/imgs/02-shop/00.png";
+    }
+    if(pos=='1'){
+        imgProducto.src = "assets/imgs/02-shop/00-2.png";
+    }
+    
+}
+
+function setMini2(pos) {
+    if(pos=='0'){
+        imgProducto.src = "assets/imgs/02-shop/01.png";
+    }
+    if(pos=='1'){
+        imgProducto.src = "assets/imgs/02-shop/01-2.png";
+    }
+}
+
+function setMini3(pos) {
+    if(pos=='0'){
+        imgProducto.src = "assets/imgs/02-shop/02.png";
+    }
+    if(pos=='1'){
+        imgProducto.src = "assets/imgs/02-shop/02-2.png";
+    }
+}
+
+function setMini4(pos) {
+    if(pos=='0'){
+        imgProducto.src = "assets/imgs/02-shop/03.png";
+    }
+    if(pos=='1'){
+        imgProducto.src = "assets/imgs/02-shop/03-2.png";
+    }
+}
+
+function setMini5(pos) {
+    if(pos=='0'){
+        imgProducto.src = "assets/imgs/02-shop/04.png";
+    }
+    if(pos=='1'){
+        imgProducto.src = "assets/imgs/02-shop/04-2.png";
+    }
+}
+
+function setMini6(pos) {
+    if(pos=='0'){
+        imgProducto.src = "assets/imgs/02-shop/05.png";
+    }
+    if(pos=='1'){
+        imgProducto.src = "assets/imgs/02-shop/05-2.png";
+    }
+}
+
+function setMini8(pos) {
+    if(pos=='0'){
+        imgProducto.src = "assets/imgs/02-shop/07.png";
+    }
+    if(pos=='1'){
+        imgProducto.src = "assets/imgs/02-shop/07-1.png";
+    }
+    if(pos=='2'){
+        imgProducto.src = "assets/imgs/02-shop/07-2.png";
+    }
+    if(pos=='3'){
+        imgProducto.src = "assets/imgs/02-shop/07-4.png";
+    }
+    if(pos=='4'){
+        imgProducto.src = "assets/imgs/02-shop/07-5.png";
+    }
+}
+
+function setMini9(pos) {
+    if(pos=='0'){
+        imgProducto.src = "assets/imgs/02-shop/08-1.png";
+    }
+    if(pos=='1'){
+        imgProducto.src = "assets/imgs/02-shop/08.png";
+    }
+    if(pos=='2'){
+        imgProducto.src = "assets/imgs/02-shop/08-2.png";
+    }
+    if(pos=='3'){
+        imgProducto.src = "assets/imgs/02-shop/08-3.png";
+    }
+    if(pos=='4'){
+        imgProducto.src = "assets/imgs/02-shop/08-6.png";
+    }
+}
---
+++ scriptGalery.js
@@ -0,0 +1,76 @@
+// Imagen que queremos modificar
+let imgTrabajo = document.getElementById("imgTrabajo");
+// Miniaturas
+let miniaturas = document.querySelectorAll(".miniaturas img")
+
+function setMini(pos){
+    if(pos=='0'){
+        imgTrabajo.src = "assets/imgs/03-trabajos/01-3.png";
+    }
+    if(pos=='1'){
+        imgTrabajo.src = "assets/imgs/03-trabajos/01-4.png";
+    }
+    if(pos=='2'){
+        imgTrabajo.src = "assets/imgs/03-trabajos/01-1.png";
+    }
+}
+
+function setMini2(pos){
+    if(pos=='0'){
+        imgTrabajo.src = "assets/imgs/03-trabajos/02-2.png";
+    }
+    if(pos=='1'){
+        imgTrabajo.src = "assets/imgs/03-trabajos/02-4.png";
+    }
+    if(pos=='2'){
+        imgTrabajo.src = "assets/imgs/03-trabajos/02-1.png";
+    }
+}
+
+function setMini3(pos){
+    if(pos=='0'){
+        imgTrabajo.src = "assets/imgs/03-trabajos/03-2.png";
+    }
+    if(pos=='1'){
+        imgTrabajo.src = "assets/imgs/03-trabajos/03-3.png";
+    }
+    if(pos=='2'){
+        imgTrabajo.src = "assets/imgs/03-trabajos/03-1.png";
+    }
+}
+
+function setMini4(pos){
+    if(pos=='0'){
+        imgTrabajo.src = "assets/imgs/03-trabajos/04-2.png";
+    }
+    if(pos=='1'){
+        imgTrabajo.src = "assets/imgs/03-trabajos/04-3.png";
+    }
+    if(pos=='2'){
+        imgTrabajo.src = "assets/imgs/03-trabajos/04-1.png";
+    }
+}
+
+function setMini5(pos){
+    if(pos=='0'){
+        imgTrabajo.src = "assets/imgs/03-trabajos/05-2.png";
+    }
+    if(pos=='1'){
+        imgTrabajo.src = "assets/imgs/03-trabajos/05-3.png";
+    }
+    if(pos=='2'){
+        imgTrabajo.src = "assets/imgs/03-trabajos/05-1.png";
+    }
+}
+
+function setMini6(pos){
+    if(pos=='0'){
+        imgTrabajo.src = "assets/imgs/03-trabajos/06-2.png";
+    }
+    if(pos=='1'){
+        imgTrabajo.src = "assets/imgs/03-trabajos/06-3.png";
+    }
+    if(pos=='2'){
+        imgTrabajo.src = "assets/imgs/03-trabajos/06-1.png";
+    }
+}
